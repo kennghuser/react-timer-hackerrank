c1fcd85d799bff3632ac05e480f2995859925485
Kenn Vun Gan <kennvun@hotmail.com>
2025-08-09 01:52:24 +0900

    refactor(timer): use useEffect to start timer countdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Timer from './pages/Timer'
 import './App.css'
 
@@ -7,16 +7,27 @@ function App() {
   const [timerValue, setTimerValue] = useState(initial);
   const interval = useRef(null);
 
-  if (timerValue > 0 && !interval.current) {
-    interval.current = setInterval(() => {
-      setTimerValue(prev => prev - 1);
-    }, 1000);
-  }
+  useEffect(() => {
+    if (timerValue > 0 && !interval.current) {
+      interval.current = setInterval(() => {
+        setTimerValue(prev => prev - 1);
+      }, 1000);
+    }
 
-  if (timerValue <= 0 && interval.current) {
-    clearInterval(interval.current);
-    interval.current = null;
-  }
+    return () => {
+      if (interval.current) {
+        clearInterval(interval.current);
+        interval.current = null;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timerValue <= 0 && interval.current) {
+      clearInterval(interval.current);
+      interval.current = null;
+    }
+  }, [timerValue]);
 
   const stopTimer = () => {
     if (interval.current) {
@@ -24,7 +35,6 @@ function App() {
       interval.current = null;
     }
   };
-
 
   return (
     <>
--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -3,7 +3,7 @@ const Timer = ({initial, stopTimer}) => {
   return (
     <div>
       <h1>Timer</h1>
-      <p>{initial}</p>
+      <p role="timer">{initial}</p>
       <button onClick={stopTimer}>Stop</button>
     </div>
   );
